de24a9c3ee37b01cd0ebbe47b3f51d822f9c7ee6
Kshitij Kulshrestha <91348714+K-Kulshrestha@users.noreply.github.com>
2023-11-30 11:42:35 -0600

    v.0

--- script.js
+++ script.js
@@ -1,81 +1,32 @@
-// Get the canvas element
-const canvas = document.getElementById("three-canvas");
-// Set up the scene, camera, and renderer
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas });
+// Array of loading messages
+const loadingMessages = [
+    "Kshitij is eagerly preparing his masterpiece!",
+    "Hold on, Kshitij is crafting something special for you...",
+    "Just a moment, Kshitij can't wait for you to explore his website!",
+    "Almost there! Kshitij is adding the final touches.",
+    "Hang tight, Kshitij is working behind the scenes to amaze you."
+];
 
-// Set renderer size to match window
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x36ba01, 0.7); // Set the color here (in this example, a dark gray color)
-
-// Add a cube to the scene (representing flames)
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 ); 
-const material = new THREE.MeshBasicMaterial({ color: 0x000000, transparent: true, opacity: 1 });
-const object = new THREE.Mesh(geometry, material);
-scene.add(object);
-
-// Create the wireframe geometry from the existing cube geometry
-const wireframeGeometry = new THREE.WireframeGeometry(geometry);
-const wireframeMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00,  transparent: true, opacity: 1});
-const wireframe = new THREE.LineSegments(wireframeGeometry, wireframeMaterial);
-
-// Add the wireframe to the scene
-scene.add(wireframe);
-
-
-// Position the camera
-camera.position.z = 30;
-camera.position.y = 2.5;
-
-// Animation loop
-function animate() {
-    requestAnimationFrame(animate);
-
-    // Rotate and scale the flames to create the effect
-    object.rotation.x += 0.01;
-    object.rotation.y += 0.01;
-    object.scale.x = 1 + Math.sin(Date.now() * 0.001) * 0.2;
-    object.scale.y = 1 + Math.cos(Date.now() * 0.001) * 0.2;
-
-    wireframe.rotation.x += 0.01;
-    wireframe.rotation.y += 0.01;
-    wireframe.scale.x = 1 + Math.sin(Date.now() * 0.001) * 0.2;
-    wireframe.scale.y = 1 + Math.cos(Date.now() * 0.001) * 0.2;
-
-    renderer.render(scene, camera);
+// Function to get a random loading message
+function getRandomLoadingMessage() {
+    const randomIndex = Math.floor(Math.random() * loadingMessages.length);
+    return loadingMessages[randomIndex];
 }
 
-// Handle window resize
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // Update canvas container height to match the content
-    const canvasContainer = document.getElementById("canvas-container");
-    canvasContainer.style.height = `${document.body.clientHeight}px`;
-});
-
-// Start the animation loop
-animate();
-
-
-// Problem: there was a canvas overflow whenever you resized the window
-// Solution: Handle window resize
-function resizeCanvasContainer() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // Update canvas container to fill the entire height of the parent
-    const canvasContainer = document.getElementById("canvas-container");
-    canvasContainer.style.height = "100%";
+// Function to update loading text
+function updateLoadingText() {
+    const loadingTextElement = document.getElementById('loading-text');
+    if (loadingTextElement) {
+        loadingTextElement.textContent = getRandomLoadingMessage();
+    }
 }
 
-// Initial call to set the canvas container height
-resizeCanvasContainer();
+// Simulate loading delay for demonstration purposes
+setTimeout(function () {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('enter-website').style.display = 'block';
+    document.body.style.overflow = 'auto'; // Restore scroll bars
+}, 5000); // Adjust the delay as needed
 
-// Handle window resize
-window.addEventListener('resize', resizeCanvasContainer);
-
+// Update loading text periodically
+setInterval(updateLoadingText, 1500);
