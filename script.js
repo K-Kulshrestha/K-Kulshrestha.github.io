da565a3a749d0599f2ef0d94f891d27db96603d1
Kshitij Kulshrestha <kshitijkuls6@gmail.com>
2023-12-27 16:52:32 -0600

    added waving cat

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Array of loading messages
 const loadingMessages = [
     "Kshitij is eagerly preparing his masterpiece!",
-    "Hold on, Kshitij is crafting something special for you...",
     "Just a moment, Kshitij can't wait for you to explore his website!",
     "Almost there! Kshitij is adding the final touches.",
-    "Hang tight, Kshitij is working behind the scenes to amaze you."
 ];
 
 // Function to get a random loading message
@@ -26,7 +24,7 @@ setTimeout(function () {
     document.getElementById('loading').style.display = 'none';
     document.getElementById('enter-website').style.display = 'block';
     document.body.style.overflow = 'auto'; // Restore scroll bars
-}, 10); // Adjust the delay as needed
+}, 2000); // Adjust the delay as needed
 
 // Update loading text periodically
-setInterval(updateLoadingText, 1500);
+setInterval(updateLoadingText, 400);
